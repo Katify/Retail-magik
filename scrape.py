f3aa2511f7950c029a6c412330a18fac04713c6a
Kat <katpopy@gmail.com>
2025-05-27 13:27:05 +0200

    added shows

--- scrape.py
+++ scrape.py
@@ -2,7 +2,7 @@ import httpx
 from selectolax.parser import HTMLParser
 
 url = "https://vidplay.top/"
-headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:137.0) Gecko/20100101 Firefox/137.0"}
+headers = {"user-agent": "mozilla/5.0 (x11; linux x86_64; rv:137.0) gecko/20100101 firefox/137.0"}
 
 response = httpx.get(url, headers=headers)
 html = HTMLParser(response.text)
@@ -13,18 +13,20 @@ def extract_text(html, sel):
    return html.css_first(sel).text()
 
 
-print("Trending Movies")
+print("Trending movies")
 for movie in movies:
 
-    item = {'name' :extract_text(movie, ".title"),
-            'quality' :(movie.css_first("span").text()),
+    item = {'Name' :extract_text(movie, ".title"),
+            'Quality' :(movie.css_first("span").text()),
             }
     print(item)
 
 print("Trending shows")
 for show in shows:
     stuff = {
-        'name': extract_text(show, ".title"),
-        'rating': extract_text(show. ".rating"),
+        'Name': extract_text(show, ".title"),
+        'rating': extract_text(show, '.rating'),
     }
+    clean_stuff = {key:value.replace("\n", "") if isinstance(value, str)else value for key, value in stuff.items()}
+    print(stuff)
 
